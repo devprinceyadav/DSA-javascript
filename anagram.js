24810c86ad0e028537d2d3b58aca1db8073d5b3c
prince <prince33thecool@gmail.com>
2022-12-29 13:51:36 +0530

    update

---
+++ anagram.js
@@ -0,0 +1,20 @@
+function checkAnangram(str1, str2) {
+  //   console.log(str1);
+  //   console.log(str2);
+  if (str1.length != str2.length) {
+    return false;
+  }
+  var object = {};
+  for (ch of str1) {
+    object[ch] = (object[ch] || 0) + 1;
+  }
+  for (ch of str2) {
+    if (!object[ch]) {
+      return false;
+    }
+    object[ch]--;
+  }
+  return true;
+}
+
+console.log(checkAnangram("princ", "ecnirp"));
